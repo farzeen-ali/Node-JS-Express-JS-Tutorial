f78966ad9ea602aa2fd847e31ecfc7d293c66696
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-06-22 21:50:53 +0500

    API with Static Data & POSTMAN

---
+++ basicAPI.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const data = require('./empData')
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'application\json'});
+    // res.write(JSON.stringify({name: 'Farzeen', dep : 'IT'}));
+    res.write(JSON.stringify(data));
+    res.end();
+}).listen(1000);
---
+++ empData.js
@@ -0,0 +1,7 @@
+const empData = [
+    {name: 'Farzeen', dep : 'IT'},
+    {name: 'Huzaifa', dep : 'HR'},
+    {name: 'Bilal', dep : 'CS'},
+];
+
+module.exports = empData;
