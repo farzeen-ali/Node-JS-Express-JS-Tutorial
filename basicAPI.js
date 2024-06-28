160649dd80997ddb0863da98d013bae64de8c786
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-06-28 22:18:22 +0500

    Done

--- basicAPI.js
+++ basicAPI.js
@@ -2,7 +2,7 @@ const http = require('http');
 const data = require('./empData')
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type' : 'application\json'});
-    // res.write(JSON.stringify({name: 'Farzeen', dep : 'IT'}));
-    res.write(JSON.stringify(data));
+    res.write(JSON.stringify({name: 'Farzeen', dep : 'IT'}));
+    // res.write(JSON.stringify(data));
     res.end();
 }).listen(1000);
