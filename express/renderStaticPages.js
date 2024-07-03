d87c040f15a6b0aa07c98f8557be8a6ad5efcf6e
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-07-03 22:06:12 +0500

    Render Static Pages

---
+++ express/renderStaticPages.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const public = path.join(__dirname, 'public');
+console.log(public);
+
+app.use(express.static(public));
+
+
+app.listen(2000);
