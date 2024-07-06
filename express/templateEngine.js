b8a86f32cad7ed3cb262f9e3d48ab02bb4a92e37
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-07-06 22:11:36 +0500

    Template Engines: EJS

---
+++ express/templateEngine.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/welcome', (req, res) => {
+    const empInfo = {
+        name : 'Farzeen',
+        id : '123'
+    }
+    res.render('Welcome', {empInfo})
+})
+
+app.listen(2000, () => {
+    console.log("Server is Running on Port No 2000")
+})
